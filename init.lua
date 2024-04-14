f4992380c0a6e51f8609e5082dca6ca65a356a6b
Lochi <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2024-04-15 09:05:08 +1000

    remove unncssary godot server thing

--- init.lua
+++ init.lua
@@ -899,10 +899,5 @@ require('lazy').setup({
     },
   },
 })
-local gdproject = io.open(vim.fn.getcwd() .. '/project.godot', 'r')
-if gdproject then
-  io.close(gdproject)
-  vim.fn.serverstart './godothost'
-end
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
